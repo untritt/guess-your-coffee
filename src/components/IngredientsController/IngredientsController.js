867af9a6d4a05fbef963db466468075221abb508
Tuyana Dondokova <tuyana.dondokova@gmail.com>
2018-09-12 15:09:04 +0800

    Implement Ingredient component

--- src/components/IngredientsController/IngredientsController.js
+++ src/components/IngredientsController/IngredientsController.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Ingredient } from "../Ingredient";
 
 export const IngredientsController = ({
   children: ingredients,
@@ -7,5 +8,19 @@ export const IngredientsController = ({
   ingredients && ingredients.length !== 0 ? (
     <fieldset>
       <legend>Add something to your cup:</legend>
+
+      {[...ingredients.keys()].map(ingredientKey => {
+        const ingredient = ingredients.get(ingredientKey);
+        return (
+          <Ingredient
+            key={ingredientKey}
+            value={ingredientKey}
+            selected={selectedIngredients.has(ingredient)}
+            name="ingredientElements"
+          >
+            {ingredient.name}
+          </Ingredient>
+        );
+      })}
     </fieldset>
   ) : null;
