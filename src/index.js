11ba759611319e9b690a96dfe32fe4c216bcd418
Tuyana Dondokova <tuyana.dondokova@gmail.com>
2018-12-14 18:22:40 +0800

    Start using redux to manage state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { beverages, ingredients } from "./digest";
+import rootReducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("content"));
+const initialState = {
+  entities: { beverages, ingredients },
+  selectedIngredients: new Set()
+};
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("content")
+);
 registerServiceWorker();
