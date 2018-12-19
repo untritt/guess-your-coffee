54cb4b5af1b5dc5f623cebdf68ae23fe089dfd99
Tuyana Dondokova <tuyana.dondokova@gmail.com>
2018-12-19 18:23:00 +0800

    Refactor state shape because of new response

---
+++ src/reducers/beverages/isFetching.js
@@ -0,0 +1,19 @@
+import {
+  FETCH_BEVERAGES_REQUEST,
+  FETCH_BEVERAGES_FAILURE,
+  FETCH_BEVERAGES_SUCCESS
+} from "../../actions";
+
+const isFetching = (state = false, { type }) => {
+  switch (type) {
+    case FETCH_BEVERAGES_REQUEST:
+      return true;
+    case FETCH_BEVERAGES_FAILURE:
+    case FETCH_BEVERAGES_SUCCESS:
+      return false;
+    default:
+      return state;
+  }
+};
+
+export default isFetching;
--- src/reducers/ingredients/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import byId from "./byId";
-import selectedIds from "./selectedIds";
-
-export default combineReducers({ byId, selectedIds });
