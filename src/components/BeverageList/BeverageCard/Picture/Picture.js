47b3bed1e068cd82d1b36fbd787994ad13136fc5
Tuyana Dondokova <tuyana.dondokova@gmail.com>
2019-01-18 16:55:28 +0800

    Remove unused component

--- src/components/BeverageList/BeverageCard/Picture/Picture.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const Picture = ({ title, source }) => (
-  <figure>
-    <img src={source} alt={title} />
-    <figcaption>{title}</figcaption>
-  </figure>
-);
